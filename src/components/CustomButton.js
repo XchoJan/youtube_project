ff17664cc670deabe2ccd0d565c1e7cb8226b785
XchoJan <harutyunyanxcho514@gmail.com>
2022-08-29 15:00:12 +0400

    new changes

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Text, TouchableOpacity, StyleSheet, View } from "react-native";
-import { color1, color2 } from "../helpers/colors";
-import { Sizes } from "../helpers/sizes";
+import { color2 } from "../helpers/colors";
 import Google from "../assets/icons/Google";
 
 const CustomButton = (props) => {
-  const {backgroundColor, borderRadius, height, fontSize, fontWeight, title, google, disabled, onPress} = props
+  const {backgroundColor, color, borderRadius, borderColor, height, fontSize, fontWeight, title, google, disabled, onPress} = props
   return (
     <TouchableOpacity
       disabled={disabled}
@@ -15,13 +14,18 @@ const CustomButton = (props) => {
           backgroundColor: backgroundColor ? backgroundColor : color2,
           height: height ? height : 50,
           borderRadius: borderRadius ? borderRadius : 15,
+          borderColor: borderColor ? borderColor : color2,
+          borderWidth: 1
         }]
       }>
       <View style={{ flexDirection: "row" }}>
         <Text style={styles.icon_box}>
           {google ? <Google /> : null}
         </Text>
-        <Text style={[styles.title, {fontWeight: fontWeight, fontSize: fontSize ? fontSize : 17}]}>
+        <Text style={[styles.title, {
+          fontWeight: fontWeight, fontSize: fontSize ? fontSize : 17,
+          color: color ? color : 'white'
+        }]}>
           {title}
         </Text>
       </View>
--- src/screens/MainScreens/Home/HomeScreen.js
+++ src/screens/MainScreens/Home/HomeScreen.js
@@ -1,135 +1,233 @@
-import React from "react";
-import { View, StyleSheet, Text, ScrollView, Image, Dimensions } from "react-native";
+import React, {useState} from "react";
+import * as Animatable from 'react-native-animatable';
+import {View, StyleSheet, Text, ScrollView, Image, ImageBackground} from "react-native";
 import MainHeader from "../../../components/MainHeader";
-import { color1, color2, color3, color4, color5, color6 } from "../../../helpers/colors";
-import { Sizes } from "../../../helpers/sizes";
+import {color1, color2, color3, color4, color5, color6} from "../../../helpers/colors";
+import {Sizes} from "../../../helpers/sizes";
 import CustomButton from "../../../components/CustomButton";
-
-const windowHeight = Dimensions.get('window').height;
+import Like from "../../../assets/icons/Like";
+import {BackgroundImage} from "react-native-elements/dist/config";
 
 const HomeScreen = (props) => {
-  console.log(windowHeight);
-  return (
-    <ScrollView>
-      <View>
-        <MainHeader app_logo/>
-      </View>
-      <View style={styles.top_wrapper}>
-        <View style={styles.title_box}>
-          <Text style={styles.title}>Today’s <Text style={{ color: color2 }}>screen time</Text></Text>
-        </View>
-        <View style={styles.timer_titles}>
-          <Text style={styles.hour_title}>Hour</Text>
-          <Text style={styles.hour_title}>Min</Text>
-        </View>
-        <View style={styles.timer_box}>
-          <Text style={styles.timer}>21:12</Text>
-        </View>
-      </View>
-      <View style={styles.bottom_wrapper}>
-        <View style={styles.coco}>
-          <View>
-            <Text style={styles.coco_description}>
-              <Text style={styles.coco_title}>Coco</Text> <Text style={{ fontSize: 20, fontWeight: "bold" }}>is
-              ready</Text> {"\n"}
-              to monitor your kid’s{"\n"}
-              screen time.
-            </Text>
-          </View>
-          <View style={{ position: "relative" }}>
-            <Image source={require("../../../assets/images/Group86.png")} />
-          </View>
-        </View>
-      </View>
-      <View style={{ marginBottom: 10}}>
-        <View>
-          <Text style={styles.enable_child_description}>Enable Child Mode before handing{"\n"}
-            this device to your kid.</Text>
-        </View>
-        <View style={{paddingHorizontal: '25%',}}>
-          <CustomButton
-            height={61}
-            borderRadius={34}
-            title={"Enable Child Mode"} />
-        </View>
-      </View>
-    </ScrollView>
-  );
+    let [animation, setAnimation] = useState(true)
+
+
+    let [fadeIn, setFadeIn] = useState({
+        from: {
+            opacity: 1,
+        },
+        to: {
+            opacity: 1,
+        },
+    })
+    let [showUp, setShopUp] = useState({
+        from: {
+            opacity: 0,
+        },
+        to: {
+            opacity: 0,
+        },
+    })
+    function handleAnimation() {
+        setAnimation(!animation)
+        if (animation) {
+            setFadeIn({
+                from: {
+                    opacity: 1,
+                },
+                to: {
+                    opacity: 0,
+                },
+            })
+            setShopUp({
+                from: {
+                    opacity: 0
+                },
+                to: {
+                    opacity: 1
+                }
+            })
+        } else {
+            setFadeIn({
+                from: {
+                    opacity: 0,
+                },
+                to: {
+                    opacity: 1,
+                },
+            })
+            setShopUp({
+                from: {
+                    opacity: 1
+                },
+                to: {
+                    opacity: 0
+                }
+            })
+        }
+
+    }
+
+    return (
+        <ScrollView>
+            <View>
+                <MainHeader app_logo/>
+            </View>
+            <View style={styles.top_wrapper}>
+
+                    <View style={styles.title_box}>
+                        <Text style={styles.title}>Today’s <Text style={{color: color2}}>screen time</Text></Text>
+                    </View>
+                    <View style={styles.timer_titles}>
+                        <Text style={styles.hour_title}>Hour</Text>
+                        <Text style={styles.hour_title}>Min</Text>
+                    </View>
+
+                <View style={{width: '100%', alignItems: 'center', top: 15}}>
+                    <View style={{backgroundColor: animation ? '#4E366E' : '#744657', width: 176, height: 120, borderRadius: 10}}>
+                        <View style={[styles.timer_box, {backgroundColor: animation ? '#A896B666' : color2}]}>
+                            <Text style={styles.timer}>21:12</Text>
+                        </View>
+                    </View>
+                </View>
+
+                <Animatable.View style={{flex: 1, width: '100%'}} animation={showUp} duration={2000}>
+                    <ImageBackground  style={{height: 250, width: '100%', top: -30}}
+                                      source={require('../../../assets/images/BackgroundBear.png')}/>
+                </Animatable.View>
+                </View>
+            <View style={styles.bottom_wrapper}>
+                <View style={styles.coco}>
+                    <Animatable.View animation={fadeIn} duration={2000}
+                                     style={styles.coco_animatable_view}>
+                        <View>
+                            <Text style={styles.coco_description}>
+                                <Text style={styles.coco_title}>Coco</Text> <Text
+                                style={{fontSize: 20, fontWeight: "bold"}}>is
+                                ready</Text> {"\n"}
+                                to monitor your kid’s{"\n"}
+                                screen time.
+                            </Text>
+                        </View>
+                        <View style={{position: "relative"}}>
+                            <Image source={require("../../../assets/images/Group86.png")}/>
+                        </View>
+                    </Animatable.View>
+                    <Animatable.View animation={showUp} duration={2000} style={styles.coco_output}>
+                        <View>
+                            <Like/>
+                        </View>
+                        <Text style={styles.coco_output_title}>
+                            You can hand over this {"\n"}device to your kid now.
+                        </Text>
+                    </Animatable.View>
+
+                </View>
+            </View>
+            <View style={{marginBottom: 10}}>
+                <View>
+                    <Text style={styles.enable_child_description}>Enable Child Mode before handing{"\n"}
+                        this device to your kid.</Text>
+                </View>
+                <View style={{paddingHorizontal: '25%',}}>
+                    <CustomButton
+                        height={61}
+                        borderRadius={34}
+                        title={animation ? "Enable Child Mode" : 'Disable child mode'}
+                        onPress={handleAnimation}
+                        backgroundColor={animation ? color2 : 'transparent'}
+                        borderColor={color2}
+                        color={animation ? 'white' : color2}
+                    />
+                </View>
+            </View>
+        </ScrollView>
+    );
 };
 
 export default HomeScreen;
 const styles = StyleSheet.create({
-  title_box: {
-    alignItems: "center",
-    paddingTop: Sizes.size80,
-  },
-  top_wrapper: {
-    backgroundColor: color5,
-    height: Sizes.size424,
-  },
-  title: {
-    fontSize: 25,
-    color: "#fff",
-  },
-  timer_titles: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    paddingTop: Sizes.size24,
-  },
-  timer_box: {
-    width: 154,
-    height: 90,
-    backgroundColor: "#71628B",
-    borderRadius: 10,
-    alignSelf: "center",
-    marginTop: Sizes.size16,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 12,
+    title_box: {
+        alignItems: "center",
+        paddingTop: Sizes.size80,
     },
-    shadowOpacity: 0.58,
-    shadowRadius: 16.00,
-    elevation: 24,
-  },
-  timer: {
-    top: 20,
-    fontSize: 28,
-    color: "#fff",
-    fontWeight: "bold",
-  },
-  hour_title: {
-    color: color6,
-  },
-  bottom_wrapper: {
-    alignItems: "center",
-    flex: 1
-  },
-  coco: {
-    width: 343,
-    height: 187,
-    borderRadius: 40,
-    backgroundColor: "#FFFCF6",
-    paddingHorizontal: 16,
-    flexDirection: "row",
-    top: -100,
-    justifyContent: "space-between",
-    paddingTop: 20,
+    top_wrapper: {
+        backgroundColor: color5,
+        height: Sizes.size424,
+    },
+    title: {
+        fontSize: 25,
+        color: "#fff",
+    },
+    timer_titles: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        paddingTop: Sizes.size24,
+        paddingHorizontal: 110
+    },
+    timer_box: {
+        width: 154,
+        height: 90,
+        borderRadius: 10,
+        alignSelf: "center",
+        marginTop: Sizes.size16,
+        alignItems: "center",
 
-  },
-  coco_description: {
-    color: color1,
-    marginTop: 40,
-  },
-  coco_title: {
-    color: color2,
-    fontSize: 22,
-  },
-  enable_child_description: {
-    textAlign: "center",
-    marginBottom: Sizes.size25,
-    color: color1,
-  },
-
+    },
+    timer: {
+        top: 20,
+        fontSize: 28,
+        color: "#fff",
+        fontWeight: "bold",
+    },
+    hour_title: {
+        color: color6,
+    },
+    bottom_wrapper: {
+        alignItems: "center",
+        flex: 1
+    },
+    coco: {
+        width: 343,
+        height: 187,
+        borderRadius: 40,
+        backgroundColor: "#FFFCF6",
+        paddingHorizontal: 16,
+        flexDirection: "row",
+        top: -100,
+        justifyContent: "space-between",
+        paddingTop: 20,
+    },
+    coco_description: {
+        color: color1,
+        marginTop: 40,
+    },
+    coco_title: {
+        color: color2,
+        fontSize: 22,
+    },
+    enable_child_description: {
+        textAlign: "center",
+        marginBottom: Sizes.size25,
+        color: color1,
+    },
+    coco_animatable_view: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%'
+    },
+    coco_output: {
+        justifyContent: "center",
+        alignItems: "center",
+        width: '100%',
+        position: "absolute",
+        left: 25,
+        top: 25
+    },
+    coco_output_title: {
+        color: '#000',
+        textAlign: "center",
+        fontSize: 17,
+        paddingTop: 20
+    }
 });
